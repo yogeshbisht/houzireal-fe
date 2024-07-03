d7facf768f3b621951a409811b401e5b0cdb670d
Yogesh Bisht <reinzire@gmail.com>
2024-07-03 15:03:02 +0530

    setup rtk query and store provider

---
+++ app/services/api.service.ts
@@ -0,0 +1,24 @@
+import { hasCookie, getCookie } from "cookies-next";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const API_DOMAIN = process.env.NEXT_PUBLIC_API_DOMAIN;
+export const API_URL = `${API_DOMAIN}/api`;
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: API_URL,
+  prepareHeaders: (headers) => {
+    headers.set("Content-Type", "application/json");
+
+    if (hasCookie("token")) {
+      headers.set("Authorization", `Bearer ${getCookie("token")}`);
+    }
+    return headers;
+  },
+  timeout: 60000,
+});
+
+export const api = createApi({
+  baseQuery,
+  tagTypes: ["UserProfile"],
+  endpoints: () => ({}),
+});
---
+++ lib/features/auth/authSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserProfile } from "@/types/user";
+
+interface AuthState {
+  profile: UserProfile | null;
+  isAuthenticated: boolean;
+}
+
+const initialState: AuthState = {
+  profile: null,
+  isAuthenticated: false,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setProfile: (state, action: PayloadAction<UserProfile>) => {
+      state.profile = action.payload;
+    },
+    setAuth: (state, action: PayloadAction<boolean>) => {
+      state.isAuthenticated = action.payload;
+    },
+  },
+});
+
+export const { setProfile, setAuth } = authSlice.actions;
+
+export default authSlice.reducer;
