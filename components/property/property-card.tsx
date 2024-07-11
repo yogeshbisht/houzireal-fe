9d6c134927a0f268ea170a06c33dccc495823e7c
Yogesh Bisht <reinzire@gmail.com>
2024-07-11 17:37:52 +0530

    property card fav icon, add to favorites feature

--- components/property/property-card.tsx
+++ components/property/property-card.tsx
@@ -2,18 +2,27 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
+import { useEffect } from "react";
+
 import { PropertyInfo } from "@/types/property";
+import {
+  getAmountWithCurrency,
+  propertyFullAddress,
+} from "@/utilities/property-utils";
+import { useGetUserProfileQuery } from "@/app/services/user.service";
+import { useAddPropertyToFavoritesMutation } from "@/app/services/property.service";
 import {
   Card,
   CardContent,
   CardTitle,
   CardHeader,
   CardDescription,
+  CardFooter,
 } from "@/components/ui/card";
-import {
-  getAmountWithCurrency,
-  propertyFullAddress,
-} from "@/utilities/property-utils";
+import { toast } from "sonner";
+import { apiErrorMessage } from "@/utilities/text-utils";
+import { cn } from "@/lib/utils";
 
 type PropertyCardProps = {
   propertyDetails: PropertyInfo;
@@ -25,6 +34,31 @@ const PropertyCard = ({
   propertyDetails,
   display = "search",
 }: PropertyCardProps) => {
+  const { data: user } = useGetUserProfileQuery();
+  const [setFavorite, { data, isLoading: isAdding, isError, error }] =
+    useAddPropertyToFavoritesMutation();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(
+        "Add To Favorites Failed",
+        apiErrorMessage(error, "Error adding property to favorites")
+      );
+    }
+    if (data && data === "success") {
+      toast.success("Property added to favorites");
+    }
+  }, [data, isError, error]);
+
+  const propertyParams = () => [
+    { label: "Type", value: propertyDetails.propertyType },
+    { label: "Size", value: `${propertyDetails.area} sqft` },
+    {
+      label: "Rooms",
+      value: `${propertyDetails.beds} beds + ${propertyDetails.baths} baths`,
+    },
+  ];
+
   const displayRibbon = () => {
     if (display === "favorites") {
       if (propertyDetails.autoSearch) {
@@ -48,14 +82,23 @@ const PropertyCard = ({
     return "https://picsum.photos/600/400";
   };
 
-  const propertyParams = [
-    { label: "Type", value: propertyDetails.propertyType },
-    { label: "Size", value: `${propertyDetails.area} sqft` },
-    {
-      label: "Rooms",
-      value: `${propertyDetails.beds} beds + ${propertyDetails.baths} baths`,
-    },
-  ];
+  const isUserFavorite = (favorites: PropertyInfo[] | undefined) => {
+    if (!favorites?.length) {
+      return false;
+    }
+
+    return favorites.some((favorite) => favorite.id === propertyDetails.id);
+  };
+
+  const onFavClick = (fav: boolean) => {
+    if (fav) {
+      // Remove from favorites
+      // eslint-disable-next-line no-alert
+      alert("Remove from favorites");
+    } else {
+      setFavorite(propertyDetails.id);
+    }
+  };
 
   return (
     <Card className="relative min-h-[400px]">
@@ -79,13 +122,37 @@ const PropertyCard = ({
         </CardDescription>
       </CardHeader>
       <CardContent>
-        {propertyParams.map((param) => (
+        {propertyParams().map((param) => (
           <div key={param.label} className="flex justify-between text-sm">
             <div className="font-medium">{param.label}</div>
             <div className="text-muted-foreground">{param.value}</div>
           </div>
         ))}
       </CardContent>
+      {user && user.id && (
+        <>
+          <hr className="w-full" />
+          <CardFooter>
+            <div
+              className={cn("flex w-full items-center justify-end pt-2", {
+                "disable-element": isAdding,
+              })}
+            >
+              {isUserFavorite(user.favorites) ? (
+                <IoMdHeart
+                  className="size-5 cursor-pointer text-red-400"
+                  onClick={() => onFavClick(true)}
+                />
+              ) : (
+                <IoMdHeartEmpty
+                  className="size-5 cursor-pointer text-red-400"
+                  onClick={() => onFavClick(false)}
+                />
+              )}
+            </div>
+          </CardFooter>
+        </>
+      )}
     </Card>
   );
 };
