a8276bab693292810e3c96dc963cf0c8aa27ca1c
Yogesh Bisht <reinzire@gmail.com>
2025-04-12 23:44:21 +0530

    update packages and fix auth bypass vulnerability

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
-
-export const isEmpty = (value: any) =>
-  value === undefined ||
-  value === null ||
-  (typeof value === "object" && Object.keys(value).length === 0) ||
-  (typeof value === "string" && value.trim().length === 0);
